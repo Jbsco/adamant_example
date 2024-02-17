7da1342485240bf37ea3b002aa2953ecb17e7ceb
Jacob Seman <jbseman@mavs.coloradomesa.edu>
2024-02-16 22:56:34 -0700

    updated README to apply helper cp script

--- gnd/cosmos/install_cosmos_plugin.sh
+++ gnd/cosmos/install_cosmos_plugin.sh
@@ -7,10 +7,10 @@
 # Ex. `./install_cosmos_plugin.sh cosmos-project` if COSMOS is adjacent to adamant_example
 
 cosmos_install_name=$1
-cosmos_install_dir=`realpath ../../../../../$cosmos_install_name`
+cosmos_install_dir=`realpath ../../../$cosmos_install_name`
 cosmos_plugin_dir=`realpath $cosmos_install_dir/openc3-cosmos-linux-example`
-adamant_example_plugin_dir=`realpath ../build/cosmos/plugin`
-adamant_protocol_dir=`realpath ../../../../../adamant/gnd/cosmos`
+adamant_example_plugin_dir=`realpath ../../src/assembly/linux/build/cosmos/plugin`
+adamant_protocol_dir=`realpath ../../../adamant/gnd/cosmos`
 echo cp $adamant_example_plugin_dir/linux_example_ccsds_cosmos_commands.txt $cosmos_plugin_dir/targets/LINUX_EXAMPLE/cmd_tlm/cmd.txt
 echo cp $adamant_example_plugin_dir/linux_example_ccsds_cosmos_telemetry.txt $cosmos_plugin_dir/targets/LINUX_EXAMPLE/cmd_tlm/tlm.txt
 echo cp $adamant_example_plugin_dir/linux_example_ccsds_cosmos_plugin.txt $cosmos_plugin_dir/plugin.txt
