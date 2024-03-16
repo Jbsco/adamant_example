e2de112d5efab508ef2e18059f1f90d2a616062e
Jbsco <90524106+Jbsco@users.noreply.github.com>
2024-03-16 00:30:39 -0600

    corrected for variation in plugin directory case & hyphenation in plugin helper script

--- gnd/cosmos/install_cosmos_plugin.sh
+++ gnd/cosmos/install_cosmos_plugin.sh
@@ -9,10 +9,10 @@
 adamant_assembly_name=$1
 cosmos_install_name=$2
 cosmos_install_dir=`realpath ../../../$cosmos_install_name`
-cosmos_plugin_dir=`realpath $cosmos_install_dir/openc3-cosmos-${adamant_assembly_name}`
+cosmos_plugin_dir=`realpath $cosmos_install_dir/openc3-cosmos-${adamant_assembly_name//_/-}`
 adamant_example_plugin_dir=`realpath ../../src/assembly/linux/build/cosmos/plugin`
-adamant_protocol_dir=`realpath $ADAMANT_DIR/gnd/cosmos`
-echo cp $adamant_example_plugin_dir/${adamant_assembly_name}_ccsds_cosmos_commands.txt $cosmos_plugin_dir/targets/LINUX_EXAMPLE/cmd_tlm/cmd.txt
-echo cp $adamant_example_plugin_dir/${adamant_assembly_name}_ccsds_cosmos_telemetry.txt $cosmos_plugin_dir/targets/LINUX_EXAMPLE/cmd_tlm/tlm.txt
+adamant_protocol_dir=`realpath ../../../adamant/gnd/cosmos`
+echo cp $adamant_example_plugin_dir/${adamant_assembly_name}_ccsds_cosmos_commands.txt $cosmos_plugin_dir/targets/${adamant_assembly_name^^}/cmd_tlm/cmd.txt
+echo cp $adamant_example_plugin_dir/${adamant_assembly_name}_ccsds_cosmos_telemetry.txt $cosmos_plugin_dir/targets/${adamant_assembly_name^^}/cmd_tlm/tlm.txt
 echo cp $adamant_example_plugin_dir/${adamant_assembly_name}_ccsds_cosmos_plugin.txt $cosmos_plugin_dir/plugin.txt
-echo cp $adamant_protocol_dir/cmd_checksum.rb $plugin_dir/targets/LINUX_EXAMPLE/lib/cmd_checksum.rb
+echo cp $adamant_protocol_dir/cmd_checksum.rb $cosmos_plugin_dir/targets/${adamant_assembly_name^^}/lib/cmd_checksum.rb
